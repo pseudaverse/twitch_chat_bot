de1e3b2a585278fc86dfcbad8f4af0191e1332e3
pseudaverse <kamil111998@gmail.com>
2020-06-09 07:06:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+DEBUG = True
+DEBUG_CHANNEL = ""
+
+WHITELIST = {}
+
+# twitch irc host and port
+HOST = "irc.twitch.tv" 
+PORT = 6667
+
+# twitch irc tags host
+TAGS_HOST = "twitch.tv/tags" 
+
+
+NICK = "" # Bot twitch nickname
+
+PASS = "" # Bot twitch oauth token
